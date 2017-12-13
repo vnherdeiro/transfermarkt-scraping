644bf3d5e7abec20c8f30feb7b9a2f7d5f03c19f
giim <victor.oriedreh@gmail.com>
2017-12-13 19:54:18 +0000

    first working version

---
+++ main.py
@@ -0,0 +1,53 @@
+#! /usr/bin/python3
+
+
+import urllib
+from bs4 import BeautifulSoup as bs
+import re
+import pandas as pd
+from time import time, sleep
+from league import League
+import pickle
+
+N_LEAGUES = 1 #ex. 10 #keeping the top N leagues
+LEAGUES_URL = "https://www.transfermarkt.co.uk/wettbewerbe/europa/wettbewerbe"
+BASE_URL = "https://www.transfermarkt.co.uk"
+
+DELAY_BETWEEN_QUERIES = 0 #min number of seconds spacing http queries
+class PageScrapper():
+    def __init__(self ):
+        self.opener = urllib.request.build_opener()
+        self.opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+        self.lastQuery = -float("inf")
+    def readUrl( self, url_):
+        presentTime = time() - self.lastQuery - DELAY_BETWEEN_QUERIES
+        if presentTime < 0:
+            sleep( abs(presentTime))
+        inData = self.opener.open(url_)
+        content = inData.read()
+        self.lastQuery = time()
+        return bs(content, "html.parser")
+    def __call__( self, url_):
+        return self.readUrl( url_)
+
+
+if __name__ == "__main__":
+	scrapper = PageScrapper()
+	soup = scrapper( LEAGUES_URL)
+	LeagueTables = soup.find("table", class_="items").find("tbody")
+	Leagues = LeagueTables.find_all("a", href=re.compile("wettbewerb/[A-Z]{2}1"), title=re.compile("\w"))
+	Leagues = Leagues[:N_LEAGUES]
+	LeagueUrlDic = { league.text : BASE_URL + league["href"] for league in Leagues}
+	print( LeagueUrlDic )
+	LeaguesData = []
+	for leagueName, leagueUrl in LeagueUrlDic.items():
+		print( "Scrapping the %s..." %leagueName)
+		LeaguesData.append( League( leagueName, leagueUrl, scrapper))
+	with open("out.dat", "wb") as f:
+		pickle.dump( LeaguesData, f)
+
+	#flattening all players information to pandas.DataFrame
+	PlayerProfiles = [player.PlayerData for league in LeaguesData for team in league.TeamsData for player in team.PlayersData]
+	df = pd.DataFrame( PlayerProfiles)
+	df.to_csv("StrikerPerformance.csv", index=False)
+	print( LeaguesData)
---
+++ team.py
@@ -0,0 +1,37 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import urllib
+from bs4 import BeautifulSoup as bs
+import re
+import pandas as pd
+from player import PlayerProfile
+
+BASE_URL = "https://www.transfermarkt.co.uk"
+
+def isStrikerOrWinger( player):
+	position = player.find_next("tr").text.strip().lower()
+	return "wing" in position or "centre-forward" in position
+
+class Team:
+	def __init__( self, url, name, scrapper):
+		self.LeagueName = name
+		soup = scrapper(url)
+		#reading player table and filtering for offensive players
+		playerTable = soup.find("table", class_="items")
+		players = playerTable.find_all("a", class_="spielprofil_tooltip")[::2]
+		offensivePlayers = filter( isStrikerOrWinger, players)
+		offensivePlayersUrls = [BASE_URL + player["href"] for player in offensivePlayers]
+		#self.PlayerData = [PlayerProfile( playerUrl, scrapper) for playerUrl in offensivePlayersUrls]
+		self.PlayersData = []
+		for playerUrl in offensivePlayersUrls:
+			#try:
+			NewPlayerProfile = PlayerProfile( playerUrl, scrapper)
+			NewPlayerProfile.PlayerData["current league"] = self.LeagueName
+			self.PlayersData.append( NewPlayerProfile)
+			#except:
+			#	continue
+
